4986279f38207fa5ddb8cdbcf133b33aded947ff
DianoDev <adrianomellocolombo@gmail.com>
2025-03-27 14:15:04 -0400

    Adição de Migrations

---
+++ database/migrations/2025_03_26_125407_create_servidor_temporarios_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servidor_temporario', function (Blueprint $table) {
+            $table->foreignId('pes_id')->primary()->constrained('pessoa', 'pes_id');
+            $table->date('st_data_admissao');
+            $table->date('st_data_demissao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servidor_temporario');
+    }
+};
